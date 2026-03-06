d391eaef588de9f1937557952971b1393b8f93f4
Wuruem Justin <mosesjustin996@gmail.com>
2026-03-07 00:11:44 +0200

    added a choice func

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 
 func main() {
 
-	var balance float64 = 0
-	var choice int
-	var amount float64
-
 	for {
 		fmt.Println("===== ATM SIMULATOR ======")
 		fmt.Println("1. Check Balance")
@@ -20,44 +16,52 @@ func main() {
 		fmt.Println("===========================")
 
 		fmt.Println("Enter your choice: ")
-		fmt.Scan(&choice)
+		choice()
+	}
+}
 
-		switch choice {
-		case 1:
-			fmt.Println("Your current balance is: ", balance)
-			fmt.Println("---------------------------")
+func choice() {
+	var balance float64 = 0
+	var choice int
+	var amount float64
 
-		case 2:
-			fmt.Println("Enter the amount to deposit: ")
-			fmt.Scan(&amount)
-			if amount > 0 {
-				balance += amount
-				fmt.Println("Amount successfully deposited:", balance)
-			} else {
-				fmt.Println("Invalid AMount")
+	fmt.Scan(&choice)
 
-			}
-			fmt.Println("---------------------------")
+	switch choice {
+	case 1:
+		fmt.Println("Your current balance is: ", balance)
+		fmt.Println("---------------------------")
 
-		case 3:
-			fmt.Println("Enter the amount to withdraw: ")
-			fmt.Scan(&amount)
-			if amount > balance {
-				fmt.Println("insufficient amount")
-
-			} else {
-				balance -= amount
-				fmt.Println("withdraw Successful: ", amount)
-			}
-			fmt.Println("---------------------------")
-
-		case 4:
-			fmt.Println("Thanks you for using our ATM.")
-			os.Exit(0)
-
-		default:
-			fmt.Println("Invalid choice. please try again.")
+	case 2:
+		fmt.Println("Enter the amount to deposit: ")
+		fmt.Scan(&amount)
+		if amount > 0 {
+			balance += amount
+			fmt.Println("Amount successfully deposited:", balance)
+		} else {
+			fmt.Println("Invalid AMount")
 
 		}
+		fmt.Println("---------------------------")
+
+	case 3:
+		fmt.Println("Enter the amount to withdraw: ")
+		fmt.Scan(&amount)
+		if amount > balance {
+			fmt.Println("insufficient amount")
+
+		} else {
+			balance -= amount
+			fmt.Println("withdraw Successful: ", amount)
+		}
+		fmt.Println("---------------------------")
+
+	case 4:
+		fmt.Println("Thanks you for using our ATM.")
+		os.Exit(0)
+
+	default:
+		fmt.Println("Invalid choice. please try again.")
+
 	}
 }
